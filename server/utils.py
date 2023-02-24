3f9424a417841a59d164be0c0d0d1e4d76e01af4
Soumya Dey <soumyadey200@hotmail.com>
2023-02-24 22:55:22 +0530

    Update transform function

--- server/utils.py
+++ server/utils.py
@@ -1,4 +1,19 @@
+import numpy as np
 from joblib import load
+import re
+
+
+def keepOnlyChars(str):
+    return " ".join(re.findall("[A-Za-z]+", str))
+
+
+def removeHtmlTgs(str):
+    return re.sub("<[^>]+>", "", str)
+
+
+def keepOnlyAscii(str):
+    return re.sub(r'[^\x00-\x7f]', r'', str)
+
 
 vectorizer = load('../model/vectorizer.joblib')
 tfidf_transformer = load('../model/tfidf.joblib')
@@ -6,6 +21,10 @@ model = load('../model/svm_linear.joblib')
 
 
 def transform_predict(data):
+    data = np.vectorize(removeHtmlTgs)(data)
+    data = np.vectorize(keepOnlyAscii)(data)
+    data = np.vectorize(keepOnlyChars)(data)
+
     data_counts = vectorizer.transform(data)
     data_tfidf = tfidf_transformer.transform(data_counts)
 
